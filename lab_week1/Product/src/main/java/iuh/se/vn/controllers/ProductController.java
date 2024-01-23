6182ed3343155d7052211fabf1933a228b6cba72
MinhDuy18 <duy19102018@gmail.com>
2024-01-23 21:42:39 +0700

    create APIGetWay and get API to Product and User

--- lab_week1/Product/src/main/java/iuh/se/vn/controllers/ProductController.java
+++ lab_week1/Product/src/main/java/iuh/se/vn/controllers/ProductController.java
@@ -1,11 +1,12 @@
 package iuh.se.vn.controllers;
 
 import iuh.se.vn.entities.Product;
+import iuh.se.vn.entities.User;
 import iuh.se.vn.repositorires.ProductRepository;
+import iuh.se.vn.repositorires.UserPeponsitory;
 import iuh.se.vn.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,10 +14,11 @@ import org.springframework.web.client.RestTemplate;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/users")
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private UserPeponsitory userPeponsitory;
 
     @Autowired
     private ProductService productService;
@@ -28,11 +30,25 @@ public class ProductController {
         return list;
     }
 
-    @GetMapping("/list")
+    @GetMapping("/users")
     public List<Map<String, Object>> getAllUsers(){
         List<Map<String, Object>> userList = productService.getAllUser();
+        List<User> userList1 = new ArrayList<>();
+
+        for (Map<String, Object> userData : userList) {
+            // Assuming you have a method to convert a map to a User entity
+            User user = mapToUser(userData);
+            userList1.add(user);
+        }
+
+        // Save all users to the repository
+        userPeponsitory.saveAll(userList1);
         return userList;
     }
 
 
+    private User mapToUser(Map<String, Object> userData) {
+        User user = new User();
+        return user;
+    }
 }
---
+++ lab_week1/Product/src/main/java/iuh/se/vn/entities/User.java
@@ -0,0 +1,21 @@
+package iuh.se.vn.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    private long id;
+    private String name;
+
+
+}
